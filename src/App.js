a80ad25de473e344058f8c2ffa1a4c2facbc45c4
Rhinoj0sa <roberto_hinojosa@hotmail.com>
2021-03-31 18:13:23 -0600

    ejemplo high order components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App({ sucursal, pago, impuesto, ganancia, calcularImpuesto }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Impuestos sucursal: {sucursal} </h1>
+        <input type='number' onChange={(e) => calcularImpuesto(e.target.value)} />
+        <p>{"Pago: " + pago}</p>
+        <p>{"Impuesto: " + impuesto}</p>
+        <p>{"Ganancia: " + ganancia}</p>
       </header>
     </div>
   );
 }
 
-export default App;
+const withImpuestos = (Component) => ({ sucursal, pago = 0, impuesto = 0.16 }) => () => {
+  const [state, setstate] = useState({
+    sucursal,
+    pago: '$' + 0,
+    impuesto: '$' + 0 + ' al ' + (impuesto * 100) + "%",
+    ganancia: ' $' + 0,
+  })
+
+  const calcularImpuesto = (pago) => {
+    setstate({
+      ...state, ...{
+        pago: '$' + pago,
+        impuesto: '$' + pago * impuesto + ' al ' + (impuesto * 100) + "%",
+        ganancia: ' $' + (pago - (pago * impuesto)),
+      }
+    })
+  }
+
+  return (<Component sucursal={state.sucursal} pago={state.pago} impuesto={state.impuesto}
+    ganancia={state.ganancia} calcularImpuesto={calcularImpuesto} />)
+
+}
+
+export default withImpuestos(App)({ sucursal: "Miguel Nieto" });
