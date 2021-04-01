69d0562eafaff391631358027e7aa2c45c483e81
Rhinoj0sa <roberto_hinojosa@hotmail.com>
2021-04-01 00:42:58 -0600

    children y clone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-
+// esto es un patron implicito de transferecia de propiedades 
+// component to be wrapped
 const App = ({ sucursal, pago, impuesto, ganancia, calcularImpuesto }) =>
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>Impuestos (HOC): {sucursal} </h1>
+      <h1>(HOC) Suc: {sucursal} </h1>
       <input type='number' onChange={(e) => calcularImpuesto(e.target.value)} />
       <p>{"Pago: " + pago}</p>
       <p>{"Impuesto: " + impuesto}</p>
@@ -15,8 +16,8 @@ const App = ({ sucursal, pago, impuesto, ganancia, calcularImpuesto }) =>
   </div>
 
 
-
-const withImpuestos = (Component) => ({ sucursal = 'Suc 00 default', pago = 0, impuesto = 0.16 }) => () => {
+// (HOC) Wrapper functional high order component ...
+const withImpuestos = (WrappedComponent) => ({ sucursal = 'Suc 00 default', pago = 0, impuesto = 0.16 }) => () => {
   const [state, setstate] = useState({
     sucursal,
     pago: '$' + 0,
@@ -34,9 +35,9 @@ const withImpuestos = (Component) => ({ sucursal = 'Suc 00 default', pago = 0, i
     })
   }
 
-  return (<Component sucursal={state.sucursal} pago={state.pago} impuesto={state.impuesto}
+  return (<WrappedComponent sucursal={state.sucursal} pago={state.pago} impuesto={state.impuesto}
     ganancia={state.ganancia} calcularImpuesto={calcularImpuesto} />)
 
 }
 
-export default withImpuestos(App)({});
+export default withImpuestos(App)({sucursal:'mis Tompiates'});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+
+//
+const Child1 = ({ mensaje, componente, actualizaState }) =>
+  <>
+    <h2 className='hijo'>{'El hijo del papa1'}</h2>
+    <p>{componente + " " + mensaje}</p>
+    <textarea rows='3' onChange={e => actualizaState(e.target.value, 'El hijo del papa 1 dice:')} />
+  </>
+const Child2 = ({ mensaje, componente, actualizaState }) =>
+  <>
+    <h2 className='hijo'>{'El hijo del papa2'}</h2>
+    <p>{componente + " " + mensaje}</p>
+    <textarea rows='3' onChange={e => actualizaState(e.target.value, 'El hijo del papa 2 dice:')} />
+  </>
+  const Child3 = ({ mensaje, componente, actualizaState }) =>
+  <>
+    <h2 className='hijo'>{'El hijo del papa3'}</h2>
+    <p>{componente + " " + mensaje}</p>
+    <textarea rows='3' onChange={e => actualizaState(e.target.value, 'El hijo del papa 3 dice:')} />
+  </>
+  const Child4 = ({ mensaje, componente, actualizaState }) =>
+  <>
+    <h2 className='hijo'>{'El hijo del papa4'}</h2>
+    <p>{componente + " " + mensaje}</p>
+    <textarea rows='3' onChange={e => actualizaState(e.target.value, 'El hijo del papa 4 dice:')} />
+  </>
+
+const Padre = ({ mensaje, componente, children: propsChildren }) => {
+  const [state, setstate] = useState({ mensaje: "Hola hijos de la...", componente: "El papa de lo hijos saluda:", })
+  const actualizaState = (mensaje, componente) => setstate({ mensaje, componente })
+  const children = React.Children.map(propsChildren,
+    child => React.cloneElement(child, { mensaje: state.mensaje, componente: state.componente, actualizaState }))
+
+  return (
+    <div className="padre">
+      <h1>{"El papa de los hijos"}</h1>
+      <p>{state.componente + ' ' + state.mensaje}</p>
+      <textarea rows='3' onChange={e => actualizaState(e.target.value, 'El papa de lo hijos dice:')} />
+      <div className='hijos'>
+        {children}
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Padre>
+      <Child1 />
+      <Child2 />
+      <Child3 />
+      <Child4 />
+    </Padre>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +62,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
