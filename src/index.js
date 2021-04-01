1b231da523fc8fc299757be111187e247c6a8c85
Rhinoj0sa <roberto_hinojosa@hotmail.com>
2021-04-01 09:30:03 -0600

    primera parte

--- src/index.js
+++ src/index.js
@@ -1,39 +1,22 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Child1 } from './components/Child1';
-import { Child2 } from './components/Child2';
-import { Child3 } from './components/Child3';
-import { Child4 } from './components/Child4';
+
 import './index.css';
 import './App.css';
 
-const Padre = ({ mensaje, componente, children: propsChildren }) => {
-  const [state, setstate] = useState({ mensaje: "Hola hijos de la...", componente: "(init state) El papa de lo hijos saluda:", })
-  const actualizaState = (mensaje, componente) => setstate({ mensaje, componente })
-  // esto es un patron implicito de transferecia de propiedades a cada hijo le agregamos las propiedades para modificar el estado del padre y el componente hijo
-  const children = React.Children.map(propsChildren,
-    child => React.cloneElement(child, { mensaje: state.mensaje, componente: state.componente, actualizaState }))
-  return (
-    <div className="padre">
-      <h1>{"El papa de los hijos"}</h1>
-      <p>{state.componente + ' ' + state.mensaje}</p>
-      <textarea rows='3' onChange={e => actualizaState(e.target.value, 'El papa de lo hijos dice:')} />
-      <div className='hijos'>
-        {children}
-      </div>
-    </div>
-  )
-}
+const Componente =({texto,numero,correo}) => 
+<>
+<p>{texto}</p>
+<p>{numero}</p>
+<p>{correo}</p>
+</>
+  
+
 
 ReactDOM.render(
   <React.StrictMode>
     <div className='App-header'>
-      <Padre >
-        <Child1 />
-        <Child2 />
-        <Child3 />
-        <Child4 />
-      </Padre>
+     <Componente texto='texto' numero='999999999' correo='abc@serc.com'/>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
